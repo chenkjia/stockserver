65cbd06bc428f23b9900e67bf9c94eb882c86c91
chenkejia <chenkjia@gmail.com>
2016-01-07 14:53:41 +0800

    拉票程序(all)

--- src/pa.js
+++ src/pa.js
@@ -4,7 +4,7 @@ var EventProxy = require('eventproxy');
 var mongoose = require('mongoose');
 var _ = require('lodash');
 
-mongoose.connect('mongodb://localhost/stock');
+mongoose.connect('mongodb://localhost/stockdemo');
 var Schema = mongoose.Schema;
 
 var historySchema = new Schema({
@@ -25,34 +25,40 @@ var stockSchema = new Schema({
 });
 
 var Stock = mongoose.model('Stock', stockSchema);
-batchGetStock(0);
-function batchGetStock (n) {
-  getStock(n*100+1,[],function(num,list,body) {
-    if (num%100==0) {
-      Stock.create(list,function (err) {
-        console.log(list);
-        console.log(list.length);
-        if(n<100){
-          batchGetStock(n+1);
+
+var ep = new EventProxy();
+batchGetStock(0,0);
+function batchGetStock (j,n) {
+  var stockRule = [0,2,200,600,601,900];
+  var m = stockRule[j]*10+n;
+  ep.after('got_stock', 100, function (list) {
+    var result = _.sortBy(_.filter(list,function(n){return n}),'code');
+    console.log(m);
+    Stock.create(result,function (err) {
+      if(n%10<9){
+        batchGetStock(j,n+1);
+      }else{
+        if(j<stockRule.length-1){
+          batchGetStock(j+1,0);
         }
-      });
-    }else{
-      getStock(num+1,list,body);
-    }
+      }
+    });
   });
+  for (var i = m*100+1; i <= m*100+100; i++) {
+    getStock(i,function (body) {
+      ep.emit('got_stock',body);
+    });
+  }
 }
+
 function pad(num,n) {
   num = num.toString();
   return Array(n>num.length?(n-(''+num).length+1):0).join(0)+num;  
 }
-function getStock (num,list,callback) {
+function getStock (num,callback) {
   var code = pad(num,6);
   get('http://stockpage.10jqka.com.cn/'+code,function(body) {
-    console.log(code);
-    if(saveStock(body)){
-      list.push(saveStock(body));
-    }
-    callback(num,list,callback);
+    callback(saveStock(body));
   });
 }
 function get(url,callback){
